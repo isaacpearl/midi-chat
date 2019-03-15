3557c5ff06fcbb320b4932dd5d1472e0a8389ed2
isaaacp <pearl.isaac@gmail.com>
2019-03-15 00:23:10 -0400

    finished barebones REST API

--- backend/app/queries.js
+++ backend/app/queries.js
@@ -1,3 +1,5 @@
+//queries.js
+
 const pool = require('../config/pool');
 
 const getUsers = (request, response) => {
@@ -9,6 +11,65 @@ const getUsers = (request, response) => {
 	});
 };
 
-module.exports = {
-	getUsers
+const getUserById = (request, response) => {
+	const id = parseInt(request.params.id);
+
+	pool.pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).json(results.rows);
+	});
 };
+
+const createUser = (request, response) => {
+	const { name, email } = request.body;
+
+	pool.pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		//TODO: figure out why this returns undefined for results.insertId
+		response.status(201).send(`User added with ID: ${results.insertId}`);
+	});
+};
+
+const updateUser = (request, response) => {
+	const id = parseInt(request.params.id);
+	const { name, email } = request.body;
+
+	pool.pool.query(
+			'UPDATE users SET name = $1, email = $2 WHERE id = $3', 
+			[name, email, id],
+			(error, results) => {
+				if(error) {
+					throw error;
+				}
+				response.status(200).send(`user modified with ID: ${id}`);
+			});
+}
+
+const deleteUser = (request, response) => {
+	const id = parseInt(request.params.id);
+	
+	pool.pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+		if (error) {
+			throw error;
+		}
+		response.status(200).send(`User deleted with ID: ${id}`);
+	});
+};
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser
+};
+
+
+
+
+
+
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+//index.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -12,7 +14,10 @@ app.get('/',(request, response) => {
 });
 
 app.get('/users', db.getUsers);
-
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(port, () => {
 	console.log(`app running on port ${port}.`);
